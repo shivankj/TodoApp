ac19c68e9038692132a15a177505b2aa9b35d361
Shivank Jaiswal <jaiswalshivank4@gmail.com>
2020-10-16 23:29:27 +0530

    buid added

---
+++ build/precache-manifest.6b1af6d9ce55808f1e5df0cbb2c8f58c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9c96a7812a1d0d867b5be9b18feffb6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8f3a94abc5e712f98015",
+    "url": "/static/js/2.3b813170.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.3b813170.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70b2e426c7a6d1373a10",
+    "url": "/static/js/main.430ad4c5.chunk.js"
+  },
+  {
+    "revision": "13e422b80bcb6f146d99",
+    "url": "/static/js/runtime-main.aeffcac1.js"
+  }
+]);
